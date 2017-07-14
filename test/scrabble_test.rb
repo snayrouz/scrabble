eabf800e830d3147552d642d4c2e73d617f70f40
snayrouz <samuel.nayrouz@gmail.com>
2017-07-13 18:18:42 -0600

    Complete: Test pass

--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -12,12 +12,41 @@ class ScrabbleTest < Minitest::Test
     assert_equal 4, game.score("f")
   end
 
+  def test_it_can_score_a_word
+    game = Scrabble.new
+
+    assert_equal 8, game.score('hello')
+  end
+
+  def test_it_can_score_empty_string
+    game = Scrabble.new
+
+    assert_equal 0, game.score("")
+  end
+
+  def test_word_can_be_nil
+    game = Scrabble.new
+
+    assert_equal 0, game.score(nil)
+  end
+
   def test_can_score_with_multipliers
     game = Scrabble.new
 
 
     assert_equal 9, game.score_with_multipliers('hello', [1,2,1,1,1])
+  end
+
+  def test_come_back_to_this
+    game = Scrabble.new
+
     assert_equal 18, game.score_with_multipliers('hello', [1,2,1,1,1], 2)
+  end
+
+  def test_come_back_to_this_one_too
+    game = Scrabble.new
+
+
     assert_equal 58, game.score_with_multipliers('sparkle', [1,2,1,3,1,2,1], 2)
   end
 
